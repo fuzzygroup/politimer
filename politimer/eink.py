27d96110547225328b739d86c531bee6211c88b3
aric <madducks@gmail.com>
2025-05-16 17:36:22 -0400

    say shenanigans one more time

--- politimer/eink.py
+++ politimer/eink.py
@@ -1,7 +1,7 @@
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
